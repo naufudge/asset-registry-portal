bf9fc556957a5d434857d0d34c83826979d99952
naufudge <naufuiii@gmail.com>
2023-10-18 09:59:44 +0500

    updated individual asset page design/layout

--- src/app/assets/[asset]/page.jsx
+++ src/app/assets/[asset]/page.jsx
@@ -3,6 +3,7 @@
 import { usePathname, useRouter } from "next/navigation";
 import axios from "axios";
 import { useEffect, Suspense, useState } from "react";
+import './asset.css'
 
 
 
@@ -44,17 +45,19 @@ export default function assetPage({ params: { assetId } }) {
     }, [])
    
     return (
-        <div className="text-center">
+        <div className="main grid text-center place-items-center">
             <Suspense fallback={<h2>Loading..</h2>}>
+                <div className="main-card mt-10 m-3 p-10 drop-shadow-lg bg-white rounded-2xl items-center justify-center">
+                  <p className="text-2xl font-bold">{results[1]}</p>
+                  <br />
+                  <h1><b>Inventory Number:</b> {results[0]}</h1>
+                  <h1><b>Location:</b> {results[2]}</h1>
+                </div>
+
                 <button 
                 onClick={() => router.back()}
-                className="rounded-full bg-purple-400 p-2 m-3 text-white"
+                className="rounded-full bg-transparent outline p-2 m-3 text-purple-400 hover:bg-purple-400 hover:text-white transform duration-300"
                 >Go back</button>
-
-
-                <p className="text-2xl">{results[1]}</p>
-                <h1><b>Inventory Number:</b> {results[0]}</h1>
-                <h1><b>Location:</b> {results[2]}</h1>
             </Suspense>
         </div>
     )
